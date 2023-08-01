ec4a135ff090bfab1317a8abcd2ba1cbad38f8fc
Ermina Trontzou <7erminatrontzou7@gmail.com>
2023-08-01 17:41:39 +0300

    Initial commit

---
+++ src/main/java/com/example/albumviewerangularspring/AlbumViewerAngularSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.albumviewerangularspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumViewerAngularSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbumViewerAngularSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/albumviewerangularspring/AlbumViewerAngularSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.albumviewerangularspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumViewerAngularSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
