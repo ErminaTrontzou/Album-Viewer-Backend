921cb90ca8ebc370d9e32d342fb36ed89f670fed
Ermina Trontzou <7erminatrontzou7@gmail.com>
2023-08-16 14:53:23 +0300

    Implemented Service for Artist Entity with tests

---
+++ src/main/java/com/example/albumviewerangularspring/services/ArtistService.java
@@ -0,0 +1,15 @@
+package com.example.albumviewerangularspring.services;
+
+import com.example.albumviewerangularspring.entities.Album;
+import com.example.albumviewerangularspring.entities.Artist;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface ArtistService {
+
+    public ResponseEntity<List<Artist>> getAllArtists();
+
+    public ResponseEntity<List<Object>> getAllArtistId();
+    public ResponseEntity<List<Album>> getAlbumsByArtist(Integer id);
+}
---
+++ src/main/java/com/example/albumviewerangularspring/services/ArtistServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.albumviewerangularspring.services;
+
+import com.example.albumviewerangularspring.entities.Album;
+import com.example.albumviewerangularspring.entities.Artist;
+import com.example.albumviewerangularspring.repositories.ArtistRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArtistServiceImpl implements ArtistService {
+
+    private final ArtistRepository artistRepository;
+
+    public ArtistServiceImpl(ArtistRepository artistRepository){
+        this.artistRepository=artistRepository;
+    }
+
+    @Override
+    public  ResponseEntity<List<Artist>> getAllArtists(){
+        return ResponseEntity.ok(artistRepository.findAll());
+    }
+
+    @Override
+    public  ResponseEntity<List<Object>> getAllArtistId(){
+        return ResponseEntity.ok(artistRepository.findArtistId());
+    }
+    @Override
+    public ResponseEntity<List<Album>> getAlbumsByArtist(Integer id){
+        return ResponseEntity.ok(artistRepository.findAllByArtistId(id));
+    }
+}
